16d614d790261d0d8a31919f7e61352cbdfd9e7f
Aurelio Amerio <aure.amerio@gmail.com>
2020-09-18 12:14:21 +0200

    up

--- install-pycall-docker.jl
+++ install-pycall-docker.jl
@@ -1,7 +1,7 @@
 using Pkg
 
 ENV["PYTHON"] = "" 
-ENV["CONDA_JL_HOME"] = "/home/amerio/miniconda3/envs/julia"
+ENV["CONDA_JL_HOME"] = "/home/$(ENV["USERNAME"])/miniconda3/envs/julia"
 
 Pkg.add("PyCall")
 Pkg.build("PyCall")
